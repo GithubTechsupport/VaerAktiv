2c6d8c8e20ce7059ab8ba2ba5dddd9d03eee0416
Philip Buhrs <phbuhrs@uio.no>
2025-03-18 13:40:15 +0100

    Added files for frost datasource and model.

---
+++ app/src/main/java/no/uio/ifi/in2000/EmptyApplication/data/frost/FrostDataSource.kt
@@ -0,0 +1,5 @@
+package no.uio.ifi.in2000.EmptyApplication.data.frost
+
+class FrostDataSource() {
+    val clientID;
+}
---
+++ app/src/main/java/no/uio/ifi/in2000/EmptyApplication/model/frost/frostInfo.kt
@@ -0,0 +1,8 @@
+package no.uio.ifi.in2000.EmptyApplication.model.frost
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class frostInfo(
+    val a: Int
+)
