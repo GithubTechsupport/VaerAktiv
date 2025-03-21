239ff244a1688f1d75b19d2af18638128b6c9a95
Aryan Al-Morshed <aryanal@ulrik.uio.no>
2025-03-21 13:38:12 +0100

    Add files via upload
    location forecast

---
+++ app/src/main/java/no/uio/ifi/in2000/EmptyApplication/data/locationforecast/LocationForecast.kt
@@ -0,0 +1,35 @@
+package no.uio.ifi.in2000.EmptyApplication.data.locationforecast
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class LocationForecast {
+
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+
+    suspend fun getResponse(url: String): LocationForecastResponse = withContext(Dispatchers.IO) {
+        val response = client.get(url)
+        val forecastResponse = Json{ignoreUnknownKeys = true}.decodeFromString<LocationForecastResponse>(response.body<String>())
+        return@withContext forecastResponse
+    }
+}
+
+suspend fun main() {
+    val loc = LocationForecast()
+    val response = loc.getResponse("https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=60&lon=11")
+    println("${response.properties.timeseries[0]}\n------------------------------------------\n${response.properties.timeseries[1]}\n------------------------------------------\n${response.properties.timeseries[2]}")
+}
