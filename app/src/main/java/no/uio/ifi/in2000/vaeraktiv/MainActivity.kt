716f14804ef3972b550dd7539c312df120d509f8
Aryan Al-Morshed <aryan123.almorshed@gmail.com>
2025-03-25 21:37:24 +0100

    Created AppModule for dependency injection Added DI for geocoderclass Created permissionsmanager Rewrote LocationDataSource to use currentlocation instead of lastlocation Rewrote MainActivity for testing purposes

--- app/src/main/java/no/uio/ifi/in2000/vaeraktiv/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/vaeraktiv/MainActivity.kt
@@ -7,7 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import no.uio.ifi.in2000.vaeraktiv.data.location.LocationDataSource
 import no.uio.ifi.in2000.vaeraktiv.ui.navbar.Navbar
 import no.uio.ifi.in2000.vaeraktiv.ui.theme.VaerAktivTheme
@@ -16,17 +21,17 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    //@Inject lateinit var locationDataSource: LocationDataSource
+    @Inject lateinit var locationDataSource: LocationDataSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("MainActivity", "onCreate called")
         super.onCreate(savedInstanceState)
-//        if (!PermissionManager.isLocationPermissionGranted(this)) {
-//            PermissionManager.requestLocationPermissions(this)
-//        } else {
-//            Log.d("MainActivity", "Location permission already granted")
-//            //Log.d("MainActivity", locationDataSource.getLastLocation().toString())
-//        }
+        Log.d("MainActivity", "onCreate called")
+        if (!PermissionManager.isLocationPermissionGranted(this)) {
+            PermissionManager.requestLocationPermissions(this)
+        } else {
+            Log.d("MainActivity", "Location permission already granted")
+            collectLastLocation()
+        }
         enableEdgeToEdge()
         setContent {
             VaerAktivTheme{
@@ -35,19 +40,33 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == PermissionManager.LOCATION_PERMISSION_REQUEST_CODE) {
-//            if (PermissionManager.isLocationPermissionGranted(this)) {
-//                Log.d("MainActivity", "Location permission granted")
-//                //Log.d("MainActivity", locationDataSource.getLastLocation().toString())
-//            } else {
-//                Log.d("MainActivity", "Location permission denied")
-//            }
-//        }
-//    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PermissionManager.LOCATION_PERMISSION_REQUEST_CODE) {
+            if (PermissionManager.isLocationPermissionGranted(this)) {
+                Log.d("MainActivity", "Location permission granted")
+                collectLastLocation()
+            } else {
+                Log.d("MainActivity", "Location permission denied")
+            }
+        }
+    }
+
+    private fun collectLastLocation() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                Log.d("MainActivity", "collectLastLocation called")
+                locationDataSource.getLastLocation().collect { location ->
+                    Log.d("MainActivity", location.toString())
+                    location?.let {
+                        Log.d("MainActivity", "Retrieved location: ${it.latitude}, ${it.longitude}")
+                    } ?: Log.d("MainActivity", "Location is null")
+                }
+            }
+        }
+    }
 }
