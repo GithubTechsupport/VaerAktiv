3e5e30c5a1d149c95984efa74fcf3a1eb5a0d2c6
matheasr <matheasr@uio.no>
2025-04-02 14:20:31 +0200

    Added button to ActivityScreen.kt

--- app/src/main/java/no/uio/ifi/in2000/vaeraktiv/ui/activity/ActivityScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/vaeraktiv/ui/activity/ActivityScreen.kt
@@ -2,10 +2,16 @@ package no.uio.ifi.in2000.vaeraktiv.ui.activity
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -15,7 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import no.uio.ifi.in2000.vaeraktiv.model.ui.Activity
 import no.uio.ifi.in2000.vaeraktiv.model.ui.ActivityDate
 import java.time.Month
@@ -50,7 +56,6 @@ fun ActivityScreen(isOnline: Boolean, viewModel: ActivityViewModel) {
                         )
                     }
 
-
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxSize()
@@ -62,6 +67,25 @@ fun ActivityScreen(isOnline: Boolean, viewModel: ActivityViewModel) {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Top
                 ) {
+                    item{
+                        OutlinedButton(
+                            onClick = {/*TODO: legg til funksjonalitet*/},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            border = BorderStroke(1.dp, Color.Black),
+                            colors = ButtonDefaults.outlinedButtonColors(
+                                contentColor = Color.Black
+                            )
+
+                        ) {
+                            Text(
+                                text = "Finn nye aktiviteter",
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier.padding(10.dp)
+                            )
+                        }
+                    }
                     item {
                         activities.forEach {
                             AddActivitiesForDay(it)
@@ -71,5 +95,4 @@ fun ActivityScreen(isOnline: Boolean, viewModel: ActivityViewModel) {
             }
         }
     }
-
 }
