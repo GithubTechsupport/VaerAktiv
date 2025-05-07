995dd539aa14b0b581793cbce087cc869b97c787
Aryan Al-Morshed <aryan123.almorshed@gmail.com>
2025-05-07 12:46:32 +0200

    Extracted JSON out of LocationForecastDataSource

--- app/src/main/java/no/uio/ifi/in2000/vaeraktiv/data/weather/locationforecast/LocationForecastDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/vaeraktiv/data/weather/locationforecast/LocationForecastDataSource.kt
@@ -11,11 +11,13 @@ import no.uio.ifi.in2000.vaeraktiv.network.httpclient.NetworkClient
 import javax.inject.Inject
 import javax.inject.Named
 
+private val json = Json { ignoreUnknownKeys = true }
+
 class LocationForecastDataSource @Inject constructor(@Named("prettyPrint-isLenient-ignoreUnknownKeys-Client") private val networkClient: NetworkClient) {
     suspend fun getResponse(url: String): LocationForecastResponse = withContext(Dispatchers.IO) {
         try {
             val response = networkClient.ktorHttpClient.get(url)
-            val content = Json{ignoreUnknownKeys = true}.decodeFromString<LocationForecastResponse>(response.body<String>())
+            val content = json.decodeFromString<LocationForecastResponse>(response.body<String>())
             return@withContext content
         } catch (e: Exception) {
             Log.e("LocationForecastDataSource", "Error getting forecast: ", e)
