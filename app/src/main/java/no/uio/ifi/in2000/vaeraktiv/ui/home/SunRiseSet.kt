fe81c9f9965f30b2abe91fc143073a060eb89d48
Niklas Gjerde <niklasgjerde@eduroam-193-157-251-108.wlan.uio.no>
2025-05-09 12:22:06 +0200

    Added a sundisplaybox file

--- app/src/main/java/no/uio/ifi/in2000/vaeraktiv/ui/home/SunRiseSet.kt
+++ app/src/main/java/no/uio/ifi/in2000/vaeraktiv/ui/home/SunRiseSet.kt
@@ -1,6 +1,5 @@
 package no.uio.ifi.in2000.vaeraktiv.ui.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -11,17 +10,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import no.uio.ifi.in2000.vaeraktiv.R
 
@@ -47,44 +42,7 @@ fun SunRiseSet(sunData : List<String>) {
                 .border(1.dp, MaterialTheme.colorScheme.background, RoundedCornerShape(cornerDp)),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Box(
-                contentAlignment = Alignment.Center
-            ) {
-                Row (
-                    modifier = Modifier.padding(vertical = 12.dp).padding(start = 12.dp)
-                ) {
-                    Column (
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.sunrise_color3),
-                            contentDescription = "Sunrise icon",
-                            modifier = Modifier.size(50.dp)
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text(
-                            text = "Sol opp",
-                            style = MaterialTheme.typography.labelSmall,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                    Spacer(modifier = Modifier.width(12.dp))
-                    Column (
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.padding(top = 20.dp)
-                    ){
-                        Text(
-                            text = sunRise,
-                            style = MaterialTheme.typography.headlineMedium,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                }
-            }
-
+            SunDisplayBox(sunRise, R.drawable.sunrise_color)
             Box(
                 modifier = Modifier
                     .height(60.dp)
@@ -93,44 +51,7 @@ fun SunRiseSet(sunData : List<String>) {
                     .align(Alignment.CenterVertically)
                     .padding(horizontal = 2.dp)
             )
-
-            Box(
-                contentAlignment = Alignment.Center
-            ) {
-                Row (
-                    modifier = Modifier.padding(vertical = 12.dp).padding(end = 12.dp)
-                ) {
-                    Column (
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.sunset_color),
-                            contentDescription = "Sunset icon",
-                            modifier = Modifier.size(50.dp)
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text(
-                            text = "Sol ned",
-                            style = MaterialTheme.typography.labelSmall,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                    Spacer(modifier = Modifier.width(12.dp))
-                    Column (
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.padding(top = 20.dp)
-                    ){
-                        Text(
-                            text = sunSet,
-                            style = MaterialTheme.typography.headlineMedium,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                }
-            }
+            SunDisplayBox(sunSet, R.drawable.sunset_color)
         }
     }
 }
