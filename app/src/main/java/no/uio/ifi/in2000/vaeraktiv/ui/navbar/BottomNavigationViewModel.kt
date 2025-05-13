4a504c9cd6080f43e8c356d7e2e6b6134a5773ae
eliseek02 <123368864+eliseek02@users.noreply.github.com>
2025-05-13 09:46:32 +0200

    Fixed navbar, so it follows best practice

---
+++ app/src/main/java/no/uio/ifi/in2000/vaeraktiv/ui/navbar/BottomNavigationViewModel.kt
@@ -0,0 +1,36 @@
+package no.uio.ifi.in2000.vaeraktiv.ui.navbar
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import no.uio.ifi.in2000.vaeraktiv.R
+
+data class NavItem(val route: String, val iconId: Int)
+
+class BottomNavigationViewModel : ViewModel() {
+    private val _navItems = listOf(
+        NavItem("location", R.drawable.location),
+        NavItem("home", R.drawable.sun),
+        NavItem("map", R.drawable.map)
+    )
+    val navItems: List<NavItem> = _navItems
+
+    private val _selectedRoute = mutableStateOf("home")
+    val selectedRoute: State<String> = _selectedRoute
+
+    fun onNavItemClick(navController: NavController, route: String) {
+        if (_selectedRoute.value == route) return
+        _selectedRoute.value = route
+        navController.navigate(route) {
+            popUpTo(navController.graph.startDestinationId)
+            launchSingleTop = true
+        }
+    }
+
+    fun updateSelectedRoute(route: String) {
+        if (_navItems.any { it.route == route }) {
+            _selectedRoute.value = route
+        }
+    }
+}
