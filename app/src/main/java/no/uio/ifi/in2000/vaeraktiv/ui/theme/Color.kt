129e8c81478dcfde18407a795fb77304913c986a
eliseek02 <123368864+eliseek02@users.noreply.github.com>
2025-05-15 13:49:12 +0200

    commented in theme folder in ui

--- app/src/main/java/no/uio/ifi/in2000/vaeraktiv/ui/theme/Color.kt
+++ app/src/main/java/no/uio/ifi/in2000/vaeraktiv/ui/theme/Color.kt
@@ -2,13 +2,14 @@ package no.uio.ifi.in2000.vaeraktiv.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
+// -- LIGHT THEME --
 //Light colors for application
 val BackgroundLight = Color(0xFFEBF9F3)
 val OnBackground = Color(0xFF335B48)
 val MonochromeLight = Color.White
 val OnSecondaryBackground = Color(0xFFA1283C)
 
-
+// -- DARK THEME --
 //Dark colors for application
 val BackgroundDark = Color(0xFF1B3C2E)
 val OnBackgroundDark = Color(0xFFC8E6C9)
